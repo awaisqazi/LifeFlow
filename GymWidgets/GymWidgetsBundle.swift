7b9e448b6ab600a0fbf0f4e34b06e62eeec3315a
Awais Qazi <awais.a.qazi@gmail.com>
2025-12-27 19:03:29 -0600

    Added GymWidgets to support LiveActivity

---
+++ GymWidgets/GymWidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  GymWidgetsBundle.swift
+//  GymWidgets
+//
+//  Created by Fez Qazi on 12/27/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct GymWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        GymWidgets()
+        GymWidgetsControl()
+    }
+}
